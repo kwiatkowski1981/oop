5efe9b38f32258876f7eb487c4ae954f4d74477c
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2021-02-28 18:17:45 +0100

    basic exercises with constructors

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,18 @@ import '../scss/main.scss';
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+// const Car = function () {};  // constructor
+function Car(){     // druga forma zapisu constructor'a
+    console.log(this);
+    this.name = name;
+}
+
+
+const maluch = new Car();
+console.log(maluch);
+
+maluch.name = 'polonez';
+
+console.log(maluch.name);
+
